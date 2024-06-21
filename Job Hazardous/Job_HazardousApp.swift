d0f7e5e96eeeba788bfeccc0d0f5c0ae4c121289
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-06-21 10:16:09 +0900

    Initial Commit

---
+++ Job Hazardous/Job_HazardousApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_HazardousApp.swift
+//  Job Hazardous
+//
+//  Created by Satoshi Mitsumori on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Job_HazardousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
